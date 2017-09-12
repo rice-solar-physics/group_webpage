1def434f842a8f1be488cff40105acc1071627c3
Will Barnes <wtb2@rice.edu>
2017-09-12 02:47:53 -0500

    pep8 stuff, remove soon

--- scripts/fetch_pubs.py
+++ scripts/fetch_pubs.py
@@ -14,22 +14,24 @@ import yaml
 
 
 class FetchPubs(object):
-    """Class for querying ADS for publication information, compiling to YAML"""
+    """
+    Class for querying ADS for publication information, compiling to YAML
+    """
 
-    def __init__(self, pub_db, ads_key=os.path.join(os.environ['HOME'],'.ads/dev_key'),  people=[], people_db='',max_pub_count_debug=1e+10):
-        """Constructor for FetchPubs class"""
-        self.journal_name_keywords = ['Astronomy', 'Astrophysics', 'Astrophysical', 'Astronomical', 'Solar', 'Royal Society','Plasmas']
+    def __init__(self, pub_db, ads_key=os.path.join(os.environ['HOME'],'.ads/dev_key'),  
+                 people=[], people_db='', max_pub_count_debug=1e+10, year_cutoff=2010):
+        self.journal_name_keywords = ['astronomy', 'astrophysics', 'astrophysical', 'astronomical', 'solar', 'royal society','plasmas']
         self._config_logger()
         self._set_key(ads_key)
         self.pub_db = pub_db
         self.max_pub_count_debug = max_pub_count_debug
+        self.year_cutoff = year_cutoff
         if people:
             self.people = people
         else:
-            self.read_people(people_db)
+            self.people = self.read_people(people_db)
 
-
-    def _set_key(self,ads_key):
+    def _set_key(self, ads_key):
         """Set token to query ADS"""
         if os.path.exists(ads_key):
             self.logger.debug('Setting ADS key from file %s'%ads_key)
@@ -43,29 +45,30 @@ class FetchPubs(object):
     def _config_logger(self):
         """Configure logger instance"""
         self.logger = logging.getLogger(type(self).__name__)
-        self.logger.setLevel(logging.DEBUG)
-        ch = logging.StreamHandler(sys.stdout)
-        ch.setLevel(logging.DEBUG)
-        self.logger.addHandler(ch)
+        #self.logger.setLevel(logging.DEBUG)
+        #ch = logging.StreamHandler(sys.stdout)
+        #ch.setLevel(logging.DEBUG)
+        #self.logger.addHandler(ch)
 
     def read_people(self,people_db):
         """Parse YAML config file containing author information"""
         with open(people_db,'r') as f:
             people = yaml.load(f)
-        self.people = []
+        people_list = []
         for p in people:
-            try:
-                self.people += p['pub_name']
-                [self.logger.info("Adding author: %s"%pn) for pn in p['pub_name']]
-            except KeyError:
-                self.logger.exception("No pub name found for %s"%p['name'])
+            if p['role'] != 'alum':
+                try:
+                    people_list += p['pub_name']
+                    self.logger.info("Adding authors: {}".format(','.join(p['pub_name'])))
+                except KeyError:
+                    self.logger.exception("No pub names found for {}".format(
+                                            ' '.join([p['first_name'],p['last_name']])))
 
+        return people_list
 
     def query_db(self):
         """Query ADS for publications by all self.people"""
-        #Initialize structure to hold all publications
         self.top_level_pubs = dict()
-        #Query db for all authors
         request_count = 0
         for a in self.people:
             self.logger.info("Querying author: %s"%a)
@@ -83,7 +86,6 @@ class FetchPubs(object):
                 for ta in t.author:
                     if ta in self.people:
                         tmp_rsp_auth.append(ta)
-                #citation count
                 try:
                     citation_count = len(t.citation)
                 except TypeError:
@@ -105,10 +107,8 @@ class FetchPubs(object):
                     self.logger.debug("Breaking at pub count %d for debugging."%self.max_pub_count_debug)
                     break
 
-
     def flatten_and_sort(self):
         """Flatten publication list and sort by year"""
-        #Flatten database
         flat_pubs = [pub for authors in self.top_level_pubs for pub in self.top_level_pubs[authors]]
         #Remove duplicates
         #TODO: a more compact way to do this; currently not very pythonic
@@ -119,70 +119,49 @@ class FetchPubs(object):
                 _tmp_titles.append(pub['title'])
                 _tmp.append(pub)
         flat_pubs = _tmp
-        #Sort by year (newest on top)
         self.flat_pubs = sorted(flat_pubs,
             key=lambda k: datetime.datetime.strptime(k['date'],'%Y-%m-%d'),
             reverse=True)
 
-
     def filter_pubs(self,paper):
         """Filter publication list"""
-        #leave out older papers
-        if int(paper.year) < 2010:
-            check_flag = False
-            return False
-
-        #Check  the journal name
         check_flag = False
         for key in self.journal_name_keywords:
             try:
-                if key in paper.pub:
+                if key in paper.pub.lower():
                     check_flag = True
                     break
             except TypeError:
                 pass
-
-        if not check_flag:
-            return check_flag
-
-        #Check if refereed publication
+        if int(paper.year) < self.year_cutoff:
+            check_flag = False
         if 'NOT REFEREED' in paper.property:
             check_flag = False
 
         return check_flag
 
-
     def pubs2yaml(self):
         """Print publication list to YAML config file"""
-        #Check for empty publication list
         if not self.flat_pubs:
             self.logger.warning("Printing empty publiction list to %s. Set publication list with self.flatten_and_sort()"%self.pub_db)
-        #Make a list of years as well
         pub_container = {'pubs':self.flat_pubs}
         pub_container['years'] = sorted(list(set(
                         [pub['year'] for pub in self.flat_pubs])),reverse=True)
-        #Print publication list to YAML file
         self.logger.info("Printing publication list to %s"%self.pub_db)
         with open(self.pub_db,'w') as yf:
             yf.write(yaml.dump(pub_container,default_flow_style=False))
-        yf.close()
 
 
-def main():
-    #Parse command line arguments
+if __name__=='__main__':
+    # Parse command line arguments
     parser = argparse.ArgumentParser(description='Query ADS database for publications from the Rice Solar Physics Research Group')
     parser.add_argument("-k","--ads_key",help="Path to file containing ADS key needed to query database")
     parser.add_argument("-d","--pub_db",help="YAML file to write publication entries to")
     parser.add_argument("--people_db",help="YAML file containing author info")
     args = parser.parse_args()
-    #Instantiate publication fetcher class
-    pub_finder = FetchPubs(args.pub_db, people=['Bradshaw, S. J.','Bradshaw, Stephen','Barnes, W. T.'], people_db=args.people_db, ads_key=args.ads_key)
-    #Query database
+    # Publication fetcher class
+    pub_finder = FetchPubs(args.pub_db, people=['Bradshaw, S. J.', 'Bradshaw, Stephen', 'Barnes, W. T.'], 
+                           people_db=args.people_db, ads_key=args.ads_key)
     pub_finder.query_db()
-    #Flatten and sort publications
     pub_finder.flatten_and_sort()
-    #Print publications to YAML
     pub_finder.pubs2yaml()
-
-if __name__=='__main__':
-    main()
